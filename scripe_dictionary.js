de02f08a733e7b3b977a83ffd59280ded8a4dbda
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2018-01-13 16:28:40 +0000

    単語の概要もスクレイピングするように

--- scripe_dictionary.js
+++ scripe_dictionary.js
@@ -6,14 +6,26 @@ module.exports = function(word) {
   return client.fetch(url)
     .then((result) => {
       const $ = result.$
+
+      // 概要をスクレイピング
+      const $descriptions = $('#resultsList > ul > li').eq(0).find('li')
+      const descriptionsNum = $descriptions.length
+      const descriptions = []
+      for (var i = 0; i < descriptionsNum; i++) {
+        descriptions.push($descriptions.eq(i).text())
+      }
+
+      // 例文を最大5個スクレイピング
       const $results = $('#resultsList > ul > li')
-      const num = $results.length
+      const sentencesNum = $results.length
       const sentences = []
-      for (var i = 2; i < num && i <= 6; i++) {
+      for (var i = 2; i < sentencesNum && i <= 6; i++) {
         var sentence = $results.eq(i).text().replace(/^(\r?\n)|\s?$/gm, '')
         sentences.push(sentence)
       }
-      return new Promise((resolve) => resolve({url,sentences}))
+
+      // 結果を通知
+      return new Promise((resolve) => resolve({url,descriptions,sentences}))
     })
     .catch((err) => new Promise((_, reject) => reject(err)))
 }
